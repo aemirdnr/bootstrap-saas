fa2bfa57cf86c7ef2de7eb672648d51c84bba99a
aemirdnr <aemirdnr@gmail.com>
2022-05-20 22:44:20 +0300

    + F.A.Q Section

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@
 $('#nav-button').click(function(){
     $('#m-nav').fadeToggle("3000");
 })
+
+// F.A.Q Collapse Animations
+$('.faq-header').click(function(){
+var text = $('.faq-headline', this);
+var icon = $('#faq-icon', this);
+
+if (!text.hasClass('active')) {
+    text.addClass('active');
+
+    icon.removeClass('bi-caret-right');
+    icon.addClass('bi-caret-down');
+    icon.addClass('active');
+}
+else {
+    text.removeClass('active');
+
+    icon.removeClass('bi-caret-down');
+    icon.addClass('bi-caret-right');
+    icon.removeClass('active');
+}
+})
